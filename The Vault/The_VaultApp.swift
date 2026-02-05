59a6b7b36169925b6609e45c3e51d0c56806fcad
Jency <arockia.jency@pranathiss.com>
2026-02-05 13:28:09 +0530

    Initial Commit

---
+++ The Vault/The_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_VaultApp.swift
+//  The Vault
+//
+//  Created by Jency on 05/02/26.
+//
+
+import SwiftUI
+
+@main
+struct The_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
